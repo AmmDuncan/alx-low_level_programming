cad5f5ca5b56935e17c8ab2d4fd2a61c6368e697
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-06-02 22:09:52 +0000

    fix puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-int main(){
-  printf("\"Programming is like building a multilingual puzzle\n");
-};
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
