458de29d65cbcaeb0842da88bf41164444ea8479
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-06-07 16:02:52 +0000

    fix put char

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,18 @@
-#include "_putchar.c"
-
-void _putchar(char[]);
+#include "main.h"
 
 /**
  * main - print _puchar
  * Return: 0
-*/
+ */
 int main(void)
 {
-	_putchar("_putchar\n");
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
 	return (0);
 }
