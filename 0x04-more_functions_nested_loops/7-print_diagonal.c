2c831d8edaca71f42ac25c7e15552034d23aa8ca
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-06-10 17:54:12 +0000

    init 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal function
+ * Description: print diagonal line in the terminal
+ * @n: number of \ that make up the line
+ */
+void print_diagonal(int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		int noOfSpaces;
+
+		for (noOfSpaces = 0; noOfSpaces < i; noOfSpaces++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+	}
+}
