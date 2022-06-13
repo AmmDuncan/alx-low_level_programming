4a5bd5945bd4b96c66048b04402217e2d0272504
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-06-13 20:29:26 +0000

    init 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: address to string to print in reverse
+ */
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+	char *first = s;
+	char *cursor = s;
+
+	while (len != 0)
+	{
+		cursor++;
+		len--;
+	}
+
+	while (cursor != first)
+	{
+		_putchar(*cursor);
+		cursor--;
+	}
+	_putchar(*first);
+	_putchar('\n');
+}
+
+/**
+ * _strlen - _strlen function
+ * @s: pointer to string to count
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 1;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+	return (i - 1);
+}
