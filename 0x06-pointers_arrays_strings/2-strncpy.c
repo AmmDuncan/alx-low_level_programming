f4e607e17e60720028f8bb4da85fd3a9922580c2
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-06-16 23:09:00 +0000

    init 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,12 +10,8 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
 	int j;
 	char *ptr = dest;
-
-	for (i = 0; dest[i] != '\0'; i++)
-		;
 
 	for (j = 0; j < n && src[j] != '\0'; j++, dest++)
 	{
