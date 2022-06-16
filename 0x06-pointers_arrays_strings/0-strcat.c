795a84d84880902f5978e09a19c96d7e89c515fc
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-06-16 21:40:33 +0000

    init 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - _strcat function
+ * Description: join two strings
+ * @dest: string to be continued
+ * @src: string to append to dest
+ * Return: char *
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	for (; *dest != '\0'; dest++)
+		;
+
+	for (i = 0; src[i] != '\0'; dest++, i++)
+	{
+		*dest = src[i];
+	}
+
+	return (dest);
+}
