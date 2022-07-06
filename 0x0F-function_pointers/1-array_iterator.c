5977e766fa80cc86da8aa76c93b920dd012aae8b
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-07-06 11:31:12 +0000

    init array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - array interator
+ * @array: array on params to iterate over
+ * @size: size of array
+ * @action: function to use during iteration
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
