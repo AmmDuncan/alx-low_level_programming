c1189e924f35bf7c95c26f8a0297e882b576a014
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-07-06 18:13:06 +0000

    try finale tired

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,5 +1,11 @@
 #ifndef CALC_H
 #define CALC_H
+int (*get_op_func(char *s))(int, int);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 /**
  * struct op - Struct op
  *
@@ -11,10 +17,4 @@ typedef struct op
 	char *op;
 	int (*f)(int, int);
 } op_t;
-int (*get_op_func(char *s))(int, int);
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
 #endif
--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -10,18 +10,19 @@
 int (*get_op_func(char *s))(int a, int b)
 {
 	op_t ops[] = {
-	    {"+", op_add},
-	    {"-", op_add},
-	    {"*", op_mul},
-	    {"/", op_div},
-	    {"%", op_mod},
-	    {NULL, NULL}};
+		{"+", op_add},
+		{"-", op_add},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
 	int i;
 
 	i = 0;
 	while (i < 5)
 	{
-		if (*(ops[i].op) == *s)
+		if (ops[i].op[0] == s[0])
 			return (ops[i].f);
 		i++;
 	}
