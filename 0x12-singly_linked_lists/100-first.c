25661eb20efca5b211489421d1b62f6b3555f7a0
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-07-14 12:25:23 +0000

    init advanced 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * run_before - function to print str before main runs
+ */
+void run_before(void)
+{
+	printf(
+	    "You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
