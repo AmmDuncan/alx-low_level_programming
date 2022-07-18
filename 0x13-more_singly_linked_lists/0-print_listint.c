0e5ba485afa0c44ee124374582e5c6c5dd023728
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-07-18 12:02:18 +0000

    init 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - print singly linked list of integers
+ * @h: head of singly linked list
+ * Return: size of printed list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+	listint_t *current = (listint_t *)h;
+
+	while (current)
+	{
+		printf("%d\n", current->n);
+		current = current->next;
+		count += 1;
+	}
+
+	return (count);
+}
