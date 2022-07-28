6d93989d9bd61531e4ad7c827c51e326b4ad4f94
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-07-28 13:02:57 +0000

    fix 3-3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,93 +1,57 @@
 #include "main.h"
+#include <stdio.h>
 
-void cant_read_file(char *from_file);
-void cant_write_to_file(char *to_file);
-void cant_close_file(char *file);
-void usage_error(void);
+#define MAXSIZE 1204
+#define SE STDERR_FILENO
 
 /**
- * main - copy content from one file to another
- * @argc: count of arguments from terminal
- * @argv: list of arguments from terminal
- * Return: int
+ * printfail - helper function to print fail message
+ * @arg: argument to be printed
+ * @flag: helps print different message
+ * Return: void
  */
-int main(int argc, char **argv)
+void printfail(char *arg, int flag)
 {
-	char *buffer = malloc(sizeof(char) * 1024);
-	char *from_file;
-	char *to_file;
-	int from_fd;
-	int to_fd;
-	int from_closed;
-	int to_closed;
-	int r;
+	if (flag == 1)
+		dprintf(SE, "Error: Can't write to %s\n", arg);
+	dprintf(SE, "Error: Can't read from file %s\n", arg);
+}
+/**
+ * main - copies content of file to another file
+ * @argc: count of arguments
+ * @argv: array of arguments
+ * Return: always true
+ */
+int main(int argc, char *argv[])
+{
+	int ifd, ofd, ist, ost;
+	char buf[MAXSIZE];
 
 	if (argc != 3)
-		usage_error();
-	from_file = argv[1];
-	to_file = argv[2];
-	from_fd = open(from_file, O_RDONLY);
-	to_fd = open(to_file, O_CREAT | O_TRUNC | O_WRONLY, 0600);
-	if (from_fd == -1)
-		cant_read_file(from_file);
-	r = read(from_fd, buffer, 1024);
-	if (r == -1)
-		cant_read_file(from_file);
-	if (to_fd == -1)
-		cant_write_to_file(to_file);
-	while (buffer != NULL)
+		dprintf(SE, "Usage: cp file_from file_to\n"), exit(97);
+	ifd = open(argv[1], O_RDONLY);
+	if (ifd == -1)
+		printfail(argv[1], 0), exit(98);
+	ofd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (ofd == -1)
+		printfail(argv[2], 1), exit(99);
+	do
 	{
-		write(to_fd, buffer, r);
-		if (r < 1024)
-			buffer = NULL;
-		else
-			r = read(from_fd, buffer, 1024);
-	}
-	from_closed = close(from_fd);
-	to_closed = close(to_fd);
-	if (from_closed == -1)
-		cant_close_file(from_file);
-	if (to_closed == -1)
-		cant_close_file(to_file);
-	free(buffer);
+		ist = read(ifd, buf, MAXSIZE);
+		if (ist == -1)
+			printfail(argv[1], 0), exit(98);
+		if (ist > 0)
+		{
+			ost = write(ofd, buf, (ssize_t)ist);
+			if (ost == -1)
+				printfail(argv[2], 1), exit(99);
+		}
+	} while (ist > 0);
+	ist = close(ifd);
+	if (ist == -1)
+		dprintf(SE, "Error: Can't close fd %d\n", ifd), exit(100);
+	ost = close(ofd);
+	if (ost == -1)
+		dprintf(SE, "Error: Can't close fd %d\n", ofd), exit(100);
 	return (0);
-}
-
-/**
- * cant_read_file - print cant read file
- * @from_file: name of file
- */
-void cant_read_file(char *from_file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from_file);
-	exit(98);
-}
-
-/**
- * cant_write_to_file - print cant read file
- * @to_file: name of file
- */
-void cant_write_to_file(char *to_file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to_file);
-	exit(99);
-}
-
-/**
- * cant_close_file - print cant read file
- * @file: name of file
- */
-void cant_close_file(char *file)
-{
-	dprintf(STDERR_FILENO, "Error: Can't close %s\n", file);
-	exit(100);
-}
-
-/**
- * usage_error - print usage error
- */
-void usage_error(void)
-{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
 }
