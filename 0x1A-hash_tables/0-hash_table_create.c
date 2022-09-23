9d8c23dc8a4cd6081472ff87565a613cd0c6b170
ammduncan <ammielgyanyawson@gmail.com>
2022-09-23 09:01:42 +0000

    add array to table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,16 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	hash_node_t *head_node;
 
 	table = malloc(sizeof(hash_table_t));
+	head_node = malloc(sizeof(hash_node_t));
 
-	if (table == NULL)
+	if (table == NULL || head_node == NULL)
 		return (NULL);
 
 	table->size = size;
+	table->array = &head_node;
 	return (table);
 }
 
