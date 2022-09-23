72770a5b64e3441695d74532212384f3385211ed
ammduncan <ammielgyanyawson@gmail.com>
2022-09-23 09:55:55 +0000

    fix 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,18 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	hash_node_t *array;
+	hash_node_t **array;
 
 	table = malloc(sizeof(hash_table_t));
-	array = malloc(sizeof(hash_node_t) * size);
 
 	if (table == NULL || !size)
 	{
-		free(array);
 		return (NULL);
 	}
 
+	array = malloc(sizeof(hash_node_t *) * size);
 	table->size = size;
-	table->array = &array;
+	table->array = array;
 
 	if (array == NULL)
 	{
