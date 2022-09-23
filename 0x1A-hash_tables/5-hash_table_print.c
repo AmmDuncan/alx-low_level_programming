40377f6be4e14f673f8505e811e50d8b7a5cf75d
ammduncan <ammielgyanyawson@gmail.com>
2022-09-23 11:43:33 +0000

    init 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print elements in a hash table
+ *
+ * @ht: table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	int first = 1;
+
+	if (ht)
+	{
+		printf("{");
+		for (i = 0; i < ht->size; i++)
+		{
+			if (ht->array[i])
+			{
+				if (!first)
+					printf(", ");
+
+				first = 0;
+				printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			}
+		}
+		printf("}\n");
+	}
+}
