761c2c771ec1b0a580621d295e4ec67871bbfc71
ammduncan <ammielgyanyawson@gmail.com>
2022-09-23 08:55:42 +0000

    init 0

---
+++ 0x1A-hash_tables/0-hasb_table_create.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Create a hash table
+ * @size: size of table
+ * Return: hash_table or NULL
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	return (table);
+}
+
