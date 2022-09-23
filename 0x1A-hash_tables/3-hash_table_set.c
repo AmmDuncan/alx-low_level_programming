7d42faacba28597c2d6931493c66d7414af55f6f
ammduncan <ammielgyanyawson@gmail.com>
2022-09-23 21:18:36 +0000

    fix 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,7 +34,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				if (!strcmp(cur->key, key))
 				{
 					cur->value = strdup(value);
-					break;
+					return (1);
 				}
 				cur = cur->next;
 			}
