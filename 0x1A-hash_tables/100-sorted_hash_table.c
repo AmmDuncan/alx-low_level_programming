a8c09f57c5979a9e18f96e39013fe842b2d36360
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-09-25 17:22:47 +0000

    try 100

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_create - Create sorted hash table
+ *
+ * @size: size of table
+ * @Return: pointer to created hash table
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *table;
+	shash_node_t **array;
+
+	table = malloc(sizeof(shash_table_t));
+
+	if (table == NULL || !size)
+	{
+		return (NULL);
+	}
+
+	array = malloc(sizeof(shash_node_t *) * size);
+	table->size = size;
+	table->array = array;
+
+	if (!array)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	return (table);
+}
