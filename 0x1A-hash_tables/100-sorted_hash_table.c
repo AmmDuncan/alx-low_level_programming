b016ea11eebc89e9c1192dba83905f8562a0920c
Ammiel Yawson <ammielgyanyawson@gmail.com>
2022-09-25 18:00:20 +0000

    fix 100

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -120,3 +120,64 @@ void set_at_index(shash_table_t *ht,
 	else
 		cur->next = node;
 }
+
+/**
+ * shash_table_get - Get value for key in hash_table
+ * @ht: table to search key in
+ * @key: key to search in table
+ * Return: value or NULL
+ */
+char *shash_table_get(const shash_table_t *ht, const char *key)
+{
+	int index;
+	shash_node_t *cur;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	cur = ht->array[index];
+
+	if (cur == NULL)
+		return (NULL);
+
+	if (!strcmp(cur->key, key))
+		return (cur->value);
+
+	while (cur != NULL)
+	{
+		if (!strcmp(cur->key, key))
+			return (cur->value);
+
+		cur = cur->next;
+	}
+
+	return (NULL);
+}
+
+/**
+ * shash_table_print - print elements in a hash table
+ *
+ * @ht: table to print
+ */
+void shash_table_print(const shash_table_t *ht)
+{
+	shash_node_t *node;
+	int first = 1;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	node = ht->shead;
+	while (node)
+	{
+		if (!first)
+			printf(", ");
+
+		first = 0;
+		printf("'%s': '%s'", node->key, node->value);
+		node = node->snext;
+	}
+	printf("}\n");
+}
